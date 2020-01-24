3784c1fad725fdf995e633105f4ff607ef599c1a
TheforbiddenGabe <gbachman20@sau66.org>
2020-01-23 20:21:17 -0500

    Auto improvements

---
+++ src/main/java/frc/robot/autocommands/Delay.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.autocommands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Delay extends CommandBase {
+
+  private Timer m_timer;
+  private double m_time;
+
+  /**
+   * Creates a new Delay.
+   */
+  public Delay(double time) {
+    m_time = time;
+    m_timer = new Timer();
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_timer.start();
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return m_timer.get() > m_time;
+  }
+}
--- src/main/java/frc/robot/autogroups/DefaultAuto.java
+++ src/main/java/frc/robot/autogroups/DefaultAuto.java
@@ -8,7 +8,9 @@
 package frc.robot.autogroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autocommands.AutoStop;
 import frc.robot.autocommands.AutoTurn;
+import frc.robot.autocommands.Delay;
 import frc.robot.autocommands.DriveStraightAuto;
 import frc.robot.subsystems.DriveTrain;
 
@@ -24,7 +26,17 @@ public class DefaultAuto extends SequentialCommandGroup {
 
             new AutoTurn(m_driveTrain, 90),
 
-            new DriveStraightAuto(m_driveTrain, .2, 2)
+            new DriveStraightAuto(m_driveTrain, .2, 2),
+            
+            new AutoStop(m_driveTrain),
+
+            new Delay(1),
+
+            new DriveStraightAuto(m_driveTrain, -.2, 2),
+
+            new AutoTurn(m_driveTrain, -90),
+
+            new DriveStraightAuto(m_driveTrain, -.2, 2)
 
         );
     }
